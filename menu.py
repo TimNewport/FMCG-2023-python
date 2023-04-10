fadb2bf074715ebdaeee22123af3b1803e6bd5d2
Reuven Lerner <reuven@lerner.co.il>
2023-04-10 18:57:32 +0300

    gitautosync update 142

--- menu.py
+++ menu.py
@@ -1,2 +1,8 @@
 def menu(*args):
-    return 'hello!'
+    while True:
+        s = input('Choose: ').strip()
+
+        if s in args:  # if the user chose a legit option
+            return s
+
+        print('Bad choice! Try again...')
