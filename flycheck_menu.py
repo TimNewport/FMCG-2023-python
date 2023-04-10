be59944eda7da0a777c43d3dceb9a776cd10780b
Reuven Lerner <reuven@lerner.co.il>
2023-04-10 18:58:16 +0300

    gitautosync update 144

--- flycheck_menu.py
+++
@@ -1,8 +0,0 @@
-def menu(*args):
-    while True:
-        s = input(f'Choose from {args}: ').strip()
-
-        if s in args:  # if the user chose a legit option
-            return s
-
-        print('Bad choice! Try again...')
